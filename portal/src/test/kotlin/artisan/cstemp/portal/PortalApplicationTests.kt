648e07bdd9161e489d6099cdedbd1a67d45ed360
Marcus Dashe <marcussdashe.developer@gmail.com>
2023-03-02 14:51:11 -0800

    We just started Artisanal Portal for C-STEMP

---
+++ portal/src/test/kotlin/artisan/cstemp/portal/PortalApplicationTests.kt
@@ -0,0 +1,13 @@
+package artisan.cstemp.portal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
